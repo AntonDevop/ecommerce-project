7418160ae9c73922770fffcd54fdd046b787632c
AntonDevop <AntonDevop@gmail.com>
2018-10-11 01:08:12 +0600

    home and contact page
    Modified home page and created basic form on the contact page

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage)
+    path('', views.home_page),
+    path('contact/', views.contact_page)
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
-def homepage(request):
-    return render(request, 'home.html')
+def home_page(request):
+    context = {
+        "title":"Home",
+        "content":"Welcome to the home page"
+    }
+    return render(request, 'home.html', context)
+
+def contact_page(request):
+    context = {
+        "title":"Home",
+        "content":"Welcome to the contact page"
+    }
+    if request.method == "POST":
+        print(request.POST.get("full_name"))
+    return render(request, 'contact.html', context)

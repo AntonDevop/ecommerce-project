155f24d768056f32d1f3e9019397e2143eb8fbb8
AntonDevop <AntonDevop@gmail.com>
2018-10-14 01:29:21 +0600

    static and media
    Done settings for local static and media files

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +27,7 @@ urlpatterns = [
     path('login/', views.login_page),
     path('register/', views.register_page)
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

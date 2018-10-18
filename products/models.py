52a5bf94bc998cf61543c4dc9d5c6d0061cc3d09
AntonDevop <AntonDevop@gmail.com>
2018-10-18 15:40:18 +0600

    ImageField fixed
    fixed mistake in renaming of an upladed file

--- products/models.py
+++ products/models.py
@@ -7,17 +7,19 @@ def get_filename_ext(filepath):
     name, ext = os.path.splitext(base_name)
     return name, ext
 
-def upload_image_path(filename):
+def upload_image_path(instance, filename):
     new_filename = random.randint(1,3910209312)
     name, ext = get_filename_ext(filename)
     final_filename = f'{new_filename}{ext}'
+    print(final_filename)
+    print(new_filename)
     return f'product/{new_filename}/{final_filename}'
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
-    image = models.ImageField(upload_to="products/", null=True, blank=True)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
     def __str__(self):
         name_price = self.title + ", price:" + str(self.price)
